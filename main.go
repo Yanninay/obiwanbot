75a16dce5c2a1f182d2b3873d0ad68502a6d0706
Yanninay <88768830+Yanninay@users.noreply.github.com>
2022-08-05 17:52:02 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"passwordgen_bot/commands"
+	"time"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+)
+
+func main() {
+
+	token := "YOUR_BOT_TOKEN"
+
+	b, err := gotgbot.NewBot(token, &gotgbot.BotOpts{
+		Client: http.Client{},
+		DefaultRequestOpts: &gotgbot.RequestOpts{
+			Timeout: 5 * time.Second,
+			APIURL:  gotgbot.DefaultAPIURL,
+		},
+	})
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	// Create updater and dispatcher.
+	updater := ext.NewUpdater(&ext.UpdaterOpts{
+		ErrorLog: nil,
+		DispatcherOpts: ext.DispatcherOpts{
+			// If an error is returned by a handler, log it and continue going.
+			Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+				fmt.Println("an error occurred while handling update:", err.Error())
+				return ext.DispatcherActionNoop
+			},
+			MaxRoutines: ext.DefaultMaxRoutines,
+		},
+	})
+
+	// Commands
+	dispatcher := updater.Dispatcher
+	dispatcher.AddHandler(handlers.NewCommand("start", commands.Start)) // /start
+	dispatcher.AddHandler(handlers.NewCommand("pass", commands.Pass))   // /pass
+	dispatcher.AddHandler(handlers.NewCommand("developers", commands.Developers))   // /help
+
+	//Start receiving updates.
+	err = updater.StartPolling(b, &ext.PollingOpts{
+		DropPendingUpdates: true,
+		GetUpdatesOpts: gotgbot.GetUpdatesOpts{
+			Timeout: 0,
+			RequestOpts: &gotgbot.RequestOpts{
+				Timeout: time.Second * 10,
+			},
+		},
+	})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+	fmt.Printf("@%s has been started...\n", b.User.Username)
+	// Idle, to keep updates coming in, and avoid bot stopping.
+	updater.Idle()
+}
